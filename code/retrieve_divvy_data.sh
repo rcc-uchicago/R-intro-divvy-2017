a0fb0977e3b1f1dfdb90810a3c6b4bca40179f08
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-07-28 13:43:49 -0500

    Various updates & improvements.

--- code/retrieve_divvy_data.sh
+++ code/retrieve_divvy_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Download and unzip the Divvy data.
+# Move to the data directory.
 cd ../data
+
+# Download and unzip the Divvy data.
 wget https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2016_Q1Q2.zip
 wget https://s3.amazonaws.com/divvy-data/tripdata/Divvy_Trips_2016_Q3Q4.zip
 unzip Divvy_Trips_2016_Q1Q2.zip
@@ -9,7 +11,7 @@ unzip Divvy_Trips_2016_Q3Q4.zip
 
 # Arrange the files a bit better and clean up (delete) files that we
 # won't need for the data analysis.
+rm -f *.zip
 mv -f Divvy_Trips_2016_Q1Q2/* .
 rmdir Divvy_Trips_2016_Q1Q2
-rm -f *.zip
 rm -f README_2016_Q3.txt README_2016_Q4.txt
