f4ee89f611b2c06487c34d4dcc7cc94d89acc6af
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-07-28 14:33:35 -0500

    Update to many files and analyses.

--- code/retrieve_divvy_data.sh
+++ code/retrieve_divvy_data.sh
@@ -15,3 +15,15 @@ rm -f *.zip
 mv -f Divvy_Trips_2016_Q1Q2/* .
 rmdir Divvy_Trips_2016_Q1Q2
 rm -f README_2016_Q3.txt README_2016_Q4.txt
+
+# Combine the trip data into a single file.
+head -n 1 Divvy_Trips_2016_Q1.csv > header.txt
+cat Divvy_Trips_2016_Q1.csv Divvy_Trips_2016_04.csv \
+    Divvy_Trips_2016_05.csv Divvy_Trips_2016_06.csv \
+    Divvy_Trips_2016_Q3.csv Divvy_Trips_2016_Q4.csv | \
+    grep -v trip_id > temp.txt
+cat header.txt temp.txt > Divvy_Trips_2016.csv
+
+# Clean up the rest of the files that are not needed.
+rm -f Divvy_Trips_2016_*.csv
+rm -f header.txt temp.txt
